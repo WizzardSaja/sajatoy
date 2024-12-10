73e1ccece8238702bbd5ba0ff17458e3a0007a88
Saja Ayesh <sajaayesh9@gmail.com>
2024-12-10 20:39:44 +0200

    racinggame

---
+++ Assets/RacingTextGame/RaceStatec.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+//public class RaceStatec : MonoBehaviour
+//{
+    public enum RaceState
+    {
+        Start,
+        Accelerate,
+        Turn,
+        Crash,
+        Finish
+    }
+    
